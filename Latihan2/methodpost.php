1dea0f8c84eb2eb026cfe11cebf09a3d5281f467
rizqimubarok_2402071 <rizqimbrk19@gmail.com>
2025-12-01 15:07:11 +0700

    Create methodpost.php

---
+++ Latihan2/methodpost.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+ <title>Formmethod GET </title>
+ </head>
+ <body>
+ <h1Input</h1>
+ <form action="methodpostproses.php" method="proses">
+ masukkan nama : <input type="text" name="nama" size="25"><input type="submit" value="proses">
+ </form>
+ </body>
+ </html>
